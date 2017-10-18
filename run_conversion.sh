0cf31e0c8405f94d3a2bfdd382dea53328f5fb95
Nick Babcock <nbabcock19@hotmail.com>
2017-10-18 09:43:36 -0400

    Add dates to run command

--- run_conversion.sh
+++ run_conversion.sh
@@ -7,7 +7,7 @@ trap "rm -rf $TMP_DIR" EXIT
 convert_file() {
     f="$1"
     fn=$(basename "$f")
-    echo "$f"
+    echo "$(date): $f"
     java -cp saxon/saxon9he.jar net.sf.saxon.Query -s:"$1" -dtd:off -q:"transform.xql" '!method=json' | \
         python3 to_csv.py > $TMP_DIR/$fn
 }
@@ -15,4 +15,6 @@ convert_file() {
 export -f convert_file
 export TMP_DIR
 find data -iname "*.xml" -print0 | xargs -0 -I{} -n1 bash -c 'convert_file {}'
+echo "$(date): Now combining all files"
 xsv cat rows $TMP_DIR/* > output.csv
+echo "$(date): Files combined, please see output.csv"
