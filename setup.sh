6a3ead78c40276b4aebf758e0fdb093de640339a
Nick Babcock <nbabcock19@hotmail.com>
2019-05-21 07:02:39 -0500

    Always grab latest saxon version on setup

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 # Download the saxon xquery java library and unzip it to the saxon directory
-curl -L "https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.8/SaxonHE9-8-0-5J.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fsaxon%2Ffiles%2FSaxon-HE%2F9.8%2F&ts=1508275190&use_mirror=superb-sea2" > saxonhe-9.8.zip
-unzip -d saxon saxonhe-9.8.zip
+curl -L "https://sourceforge.net/projects/saxon/files/latest/download" > saxon9he.zip
+unzip -d saxon saxon9he.zip
+rm saxon9he.zip
 
 # Store all the register data under the 'data' directory
 mkdir -p data
